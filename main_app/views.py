93f66d392e7282d14652f4f43d0c921c5ee9ac83
Trofim <trofa13@inbox.ru>
2016-11-09 17:57:50 +0300

    implemented model

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Treasure
 
 
 # Create your views here.
 def index(request):
+	treasures = Treasure.objects.all()
 
 	return render(request, 'index.html', {'treasures': treasures})
 
@@ -11,16 +13,16 @@ def index(request):
 
 
 
-class Treasure:
-	def __init__(self, name, value, material, location):
-		self.name = name
-		self.value = value
-		self.material = material
-		self.location = location
+# class Treasure:
+# 	def __init__(self, name, value, material, location):
+# 		self.name = name
+# 		self.value = value
+# 		self.material = material
+# 		self.location = location
 
 
-treasures = [
-	Treasure('Gold Nugget', 500.00, 'gold', 'Boobs inc.'),
-	Treasure('Silver Nugget', 40.05, 'silver', 'Acme'),
-	Treasure('Nickel', 0, 'tin', 'Spam inc.')
-]
+# treasures = [
+# 	Treasure('Gold Nugget', 500.00, 'gold', 'Boobs inc.'),
+# 	Treasure('Silver Nugget', 40.05, 'silver', 'Acme'),
+# 	Treasure('Nickel', 0, 'tin', 'Spam inc.')
+# ]
