a13d1517faa052dd262f0a50d892965856e5dbe3
Trofim <trofa13@inbox.ru>
2016-11-10 10:52:08 +0300

    implemented meta forms

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Treasure
 from .forms import TreasureForm
 
@@ -19,12 +19,7 @@ def detail(request, treasure_id):
 def post_treasure(request):
 	form = TreasureForm(request.POST)
 	if form.is_valid():
-		treasure = Treasure(name = form.cleaned_data['name'],
-							value = form.cleaned_data['value'],
-							material = form.cleaned_data['material'],
-							location = form.cleaned_data['location'],
-							img_url = form.cleaned_data['img_url'])
-		treasure.save()
+		form.save(commit = True)
 
 	return HttpResponseRedirect('/')
 
