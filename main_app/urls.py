a4b07a87650e8514533be310f4a2ebaa48189e75
Trofim <trofa13@inbox.ru>
2016-11-10 10:39:45 +0300

    implemented form

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index),
-	url(r'^([0-9]+)/$', views.detail, name='detail')
+	url(r'^$', views.index, name='index'),
+	url(r'^([0-9]+)/$', views.detail, name='detail'),
+	url(r'^post_url/$', views.post_treasure, name='post_treasure')
 ]
